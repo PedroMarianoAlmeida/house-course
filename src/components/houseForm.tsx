5fce9a148e438a2fc8aa804ff1587ac161846e04
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-02-11 16:10:36 -0300

    Class 9

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
 // import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -31,6 +31,8 @@ export default function HouseForm({ }: IProps) {
         defaultValues: {}
     });
 
+    const address = watch("address");
+
     useEffect(() => {
         register({ name: "address" }, { required: "Please enter your address" });
         register({ name: "latitude" }, { required: true, min: -90, max: 90 });
@@ -56,10 +58,17 @@ export default function HouseForm({ }: IProps) {
                     Search for your address
                 </label>
 
-                {/*SEARCH FIELD */}
+                <SearchBox
+                    onSelectAddress={(address, latitude, longitude) => {
+                        setValue("address", address);
+                        setValue("latitude", latitude);
+                        setValue("longitude", longitude);
+                    }}
+                    defaultValue=""
+                />
 
                 {errors.address && <p>{errors.address.message}</p>}
-
+                <h2>{address}</h2>
 
             </div>
         </form>
